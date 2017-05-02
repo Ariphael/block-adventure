b8d694da12695475f51e42b1426a0008e7636c9c
Ariphael <richard.tochtermann@hotmail.com>
2017-05-02 22:01:52 +1000

    Implemented Location Class specifying grid coordinates

---
+++ Engine/Location.cpp
@@ -0,0 +1,90 @@
+#include "Location.h"
+
+
+
+Location::Location(float x, float y)
+	:
+	x(x),
+	y(y)
+{
+}
+
+float Location::GetPosX()const
+{
+	return x;
+}
+
+float Location::GetPosY()const
+{
+	return y;
+}
+
+float Location::GetRealPosX() const
+{
+	return x*GridDimensions;
+}
+
+float Location::GetRealPosY() const
+{
+	return y*GridDimensions;
+}
+
+float Location::GetDimensions() const
+{
+	return GridDimensions;
+}
+
+Location Location::operator+(Location & rhs) const
+{
+	return Location(x + rhs.x, y + rhs.y);
+}
+
+void Location::operator+=(Location & rhs)
+{
+	x += rhs.x;
+	y += rhs.y;
+}
+
+Location Location::operator-(Location & rhs) const
+{
+	return Location(x - rhs.x, y - rhs.y);
+}
+
+void Location::operator-=(Location & rhs)
+{
+	x -= rhs.x;
+	y -= rhs.y;
+}
+
+Location Location::operator*(Location & rhs) const
+{
+	return Location(x*rhs.x,y*rhs.y);
+}
+
+void Location::operator*=(Location& rhs)
+{
+	x *= rhs.x;
+	y *= rhs.y;
+}
+
+Location Location::operator/(Location & rhs) const
+{
+	return Location(x/rhs.x,y/rhs.y);
+}
+
+void Location::operator/=(Location & rhs)
+{
+	x /= rhs.x;
+	y /= rhs.y;
+}
+
+void Location::operator=(Location & rhs)
+{
+	x = rhs.x;
+	y = rhs.y;
+}
+
+
+Location::~Location()
+{
+}
---
+++ Engine/Location.h
@@ -0,0 +1,27 @@
+#pragma once
+class Location
+{
+public:
+	Location()=default;
+	Location(float x, float y);
+	float GetPosX()const; //Returns position of x in the grid.
+	float GetPosY()const; //Returns position of y in the grid.
+	float GetRealPosX()const; //Returns absolute position of x in window.
+	float GetRealPosY()const; //Returns absolute position of y in window.
+	float GetDimensions()const;
+	Location operator+(Location& rhs)const;
+	void operator+=(Location& rhs);
+	Location operator-(Location& rhs)const;
+	void operator-=(Location& rhs);
+	Location operator*(Location& rhs)const;
+	void operator*=(Location& rhs);
+	Location operator/(Location& rhs)const;
+	void operator/=(Location& rhs);
+	void operator=(Location& rhs);
+	~Location();
+private:
+	float x;
+	float y;
+	const float GridDimensions = 10;
+};
+
